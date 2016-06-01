8d5e89f8b6a492353d616d7784fd5252ced623c1
Sarah Suchá <sarah_deutschland@hotmail.com>
2016-06-01 15:16:18 -0700

    create two methods for arrays for release 2

--- ruby/arrays_drill.rb
+++ ruby/arrays_drill.rb
@@ -1,3 +1,20 @@
+#Release 2
+
+def add_to_array(array, item)
+  array << item
+end
+
+p add_to_array([], "a")
+p add_to_array(["a", "b", "c", 1, 2], 3)
+
+def build_array(item1,item2,item3)
+  [item1, item2, item3]
+end
+
+p build_array(1,"two", nil)
+
+#Release 1
+
 p arr = []
 p arr += ["one", 2, false, "hi", nil]
 arr.delete_at(2)
@@ -10,3 +27,6 @@ p "This array includes the word 'hi'"
 p arr.include?("hi")
 arr2 = ["bye", "red", "blue"]
 p arr3 = arr + arr2
+
+
+# Pair: Millie Chan and Sarah Sucha

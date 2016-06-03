c6b7a2da4d91fbac515c83f6ad74845f934e07db
Sarah Suchá <sarah_deutschland@hotmail.com>
2016-06-03 09:47:19 -0700

    pseudocode for release 3

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,1 +1,12 @@
-#ask the user for a variety of information: name (string), email (string), phone number (string), how many years has worked as an interior designer (number),  
+#ask the user for a variety of information:
+#     name (string),
+#     email (string),
+#     phone number (string),
+#     how many years has worked as an interior designer (number),
+#     likes Moroccan design (boolean),
+#     likes Hungarian design (boolean),
+#     likes Otomi Embroidery (boolean),
+#     likes modern art (boolean)
+# get user input and convert to correct data type
+# ask user if happy with input or would like to update
+#

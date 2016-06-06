8a499ae4e2349596f71bfa0818c9befab6789dbf
Sarah Suchá <sarah_deutschland@hotmail.com>
2016-06-06 12:06:25 -0700

    describe in comments puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,15 @@
+# SPECIES -----------------------------
+# Canine Preciousoleus
+#
+# CHARACTERISTICS ---------------------
+# Eye size: large
+# Friendly: yes
+# Leg count: 4
+# Name: varies
+# Color: varies
+#
+# BEHAVIOR ----------------------------
+# Pees (often where not supposed to)
+# Cuddles
+# Plays
+# Chews

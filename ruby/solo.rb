88cb1c10b94204f49ef0f1e7d2f997fea44a006d
Sarah Suchá <sarah_deutschland@hotmail.com>
2016-06-08 16:49:39 -0700

    pseudocode design for Rabbit Guide book class

---
+++ ruby/solo.rb
@@ -0,0 +1,22 @@
+#PSEUDOCODE
+#create a CLASS for Rabbit Guides books
+#define book title
+#define book price
+#define book publishing date
+#define book base cost
+#make it possible to publish the book about a specific place
+  #store the book in a list of published books
+  #assign the book an ISBN number
+#make it possible for someone to buy the book
+  #get the name of the customer
+  #get how many they want to buy
+  #get source from where bought book
+#calculate profits for each book sold
+  #money from book sold - base cost / 2
+
+#create list of ISBN numbers
+#create list of sale channels and price per channel
+
+#BUSINESS LOGIC
+
+#DRIVER CODE

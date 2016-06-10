0fa3b410874ba1016a198b684fafb26c7cd629e5
Sarah Suchá <sarah_deutschland@hotmail.com>
2016-06-10 12:35:29 -0700

    create alias cipher and break into separate method and put user interface into method with driver code to call

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,54 +1,61 @@
-# https://repl.it/CZrB/1
-
-#write a method that takes the first and last name and creates alias
-def makeAlias(name)
+# write a method that includes the logic for which letter to replace a letter with
+	# IF a vowel, go to next vowel
+		# u = a
+	# IF a consonant has a vowel after it, skip to next consonant
+		# z = b
+	# ELSE all consonents go to next consontant
+# write a method that takes a name and creates alias
   # lowercase name so that it will be easy to work with
-  #name.downcase
-  # swap first and last name as an array and then paste back together again into a string
-  swapName = name.downcase.split(' ').reverse!.join(' ')
-  # get length of string so that can parse through each letter
-  nameLength = swapName.length
-  #define consontants and vowels strings
-  consonants= 'bcdfghjklmnpqrstvwxyz'.split('')
-  vowels = 'aeiou'.split('')
-  aliasName = ""
-  # define where to start in the make alias process (at index 0)
-  counter = 0
-  while counter < swapName.length
-    case
-    # if a space, remains a space
-    when swapName[counter] == ' '
-      newLetter = ' '
-    #if letter is found in consonants, push to next letter in consonants string
-    when consonants.include?(swapName[counter]) == true
-      newLetter = consonants[(consonants.index(swapName[counter]) +1) % 21]
-    #if letter is found in vowels, push to next letter in vowels string
-    when vowels.include?(swapName[counter]) == true
-      newLetter = vowels[(vowels.index(swapName[counter])+1) % 5]
+  # swap first and last name
+   # parse through each letter
+   	# IF a space, remains a space
+   	# ELSE use method to replace letter
+
+def find_alias_letter(letter)
+	cipher = {'a' => 'e',  'e' => 'i', 'i' => 'o', 'o' => 'u', 'u' => 'a', 'z' => 'b'}
+	if cipher.has_key?(letter)
+      	letter = cipher[letter]
+    elsif letter == 'd' || letter == 'h' || letter == 'n' || letter == 't'
+		letter = letter.next.next
+    else
+    	letter = letter.next
     end
-    aliasName += newLetter
-    counter += 1
+end
+
+def make_alias(name)
+  swap_name = name.downcase.split(' ').reverse!.join(' ')
+  alias_name = ""
+  swap_name.each_char do |letter|
+  	if letter == ' '
+  		alias_name += ' '
+  	else
+  		alias_name += find_alias_letter(letter)
+  	end
   end
-  return alaisName = aliasName.split.map(&:capitalize).join(' ')
+  return alais_name = alias_name.split.map(&:capitalize).join(' ')
 end
 
 # User Interface
-# user can put in as many names as wish to make aliases for
-addNames = ''
+
+# create method for user to put in as many names as wish to make aliases for
 # set up a hash in which to save requested names and thier aliases
-aliasList = {}
-until addNames == 'quit'
-  puts "Write a name to create an alias: (when finished, enter 'quit')"
-  aliasRequest = gets.chomp
-  if aliasRequest == 'quit'
-    # print out entered names with respective alias names
-    addNames = 'quit'
-  else
+	# print out entered names with respective alias names
     # save names entered into a hash with requested name as key and alias name as value
-    aliasList[aliasRequest] = makeAlias(aliasRequest)
-  end
-end
 # print out all requested names and their aliases
-aliasList.each do |originalName, aliasName|
-  puts "The alias for #{originalName} is #{aliasName}."
+
+def request_alias()
+	alias_list = {}
+	loop do
+		puts "Write a name to create an alias: (when finished, enter 'quit')"
+		alias_request = gets.chomp
+		break if alias_request == "quit"
+		alias_list[alias_request] = make_alias(alias_request)
+	end
+	alias_list.each do |original_name, alias_name|
+		puts "The alias for #{original_name} is #{alias_name}."
+	end
 end
+
+# Driver Code
+# call user interface
+request_alias()

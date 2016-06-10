307f006175a882949b749989b40e9a21f61b38c0
Sarah Suchá <sarah_deutschland@hotmail.com>
2016-06-10 09:56:09 -0700

    create angry and happy methods

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,13 @@
 module Shout
+  def self.yell_angrily(words)
+  words + "!!!" + " :("
+  end
 
-end 
+  def self.yelling_happily(words)
+    ":P " + words + " :) :D"
+  end
+
+end
+
+p Shout.yell_angrily("I'm so angry")
+p Shout.yelling_happily("Life is good")

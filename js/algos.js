d677343626bc8921be9bbe6483e0215ee7265001
Sarah Suchá <sarah_deutschland@hotmail.com>
2016-06-15 08:28:28 -0700

    Create function to find longest phrase in set of phrases

--- js/algos.js
+++ js/algos.js
@@ -1,7 +1,20 @@
 // PSEUDOCODE
-// Loop through each phrase in a set of phrases
-// Determine the length of each phrase
-// Return the longest phrase   
+// Write a FUNCTION that takes in a set of phrases.
+//    Loop through each phrase in a set of phrases.
+//      Determine the length of each phrase.
+//    Return the longest phrase.
 
-def longest(string)
-end
+function longestPhrase(phraseGroup) {
+	var longest = "";
+	for (var i = 0; i < phraseGroup.length; i++) {
+	    if (longest.length < phraseGroup[i].length) {
+	      longest = phraseGroup[i];
+	    }
+	}
+	return longest;
+}
+
+var phrases = longestPhrase(["long phrase","longest phrase","longer phrase"]);
+console.log(phrases)
+var newPhrases = longestPhrase(["JavaScript", "Ruby", "Node", "MySQL"]);
+console.log(newPhrases)

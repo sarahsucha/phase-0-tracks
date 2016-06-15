d0380889e2b9e59f8159fa9200f4a154d4411b6f
Sarah Suchá <sarah_deutschland@hotmail.com>
2016-06-14 17:28:32 -0700

    Write function to reverse a string

--- js/explore.js
+++ js/explore.js
@@ -5,3 +5,16 @@
   // Push the last letter to the new reversed word
   // IF there is a space, keep the space
 // Print out the reversed string
+
+function reverseString(string) {
+  reverse_string = "";
+  for (var i = string.length -1; i >= 0; i-=1) {
+    reverse_string += string[i];
+  };
+  console.log(reverse_string);
+}
+
+var testString = "JavaScript";
+reverseString("JavaScript");
+
+//console.log(testString.length)

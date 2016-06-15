f594c993ab9a25c221081323b7ed756da6ff061e
Sarah Suchá <sarah_deutschland@hotmail.com>
2016-06-14 17:07:28 -0700

    Write pseudocode for function to reverse a string

---
+++ js/explore.js
@@ -0,0 +1,7 @@
+// PSEUDOCODE
+// Get a string
+// Reverse the letters of the string
+  // Loop through each letter
+  // Push the last letter to the new reversed word
+  // IF there is a space, keep the space
+// Print out the reversed string

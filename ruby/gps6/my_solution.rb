0eafad76d86442045983103ab177813e78b7bd5a
Sarah Suchá <sarah_deutschland@hotmail.com>
2016-06-16 14:12:43 -0700

    Add comments to answer questions in reflect section

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -1,16 +1,15 @@
 # Virus Predictor
 
-# I worked on this challenge [by myself, with: ].
-# We spent [#] hours on this challenge.
+# I worked on this challenge with Stephen Fang.
+# We spent 2 hours on this challenge.
 
 # EXPLANATION OF require_relative
-#
+# require_relative takes the file from the local / relative folder and inputs the called file into the current file. Require may be useful if the file isn't relative and you need to use an absolute path.
 #
 #population density is number of people per square mile as of 2012
 #this data is updated every year with estimates from a 10 year census
 
 require_relative 'state_data'
-# require_relative takes the file from the local / relative folder and inputs the called file into the current file. Require may be useful if the file isn't relative and you need to use an absolute path.
 
 class VirusPredictor
 
@@ -80,29 +79,44 @@ end
 # DRIVER CODE
  # initialize VirusPredictor for each state
 
-# STATE_DATA.each do |state_name, state_info|
-#   # p state_info
+STATE_DATA.each do |state_name, state_info|
+  state = VirusPredictor.new(state_name, state_info[:population_density], state_info[:population])
+  state.virus_effects
+ end
 
-#   state = VirusPredictor.new(state_name, state_info[:population_density], state_info[:population])
-#   state.virus_effects
-#  #p state_info[:population_density]
-
-#  end
-
-
-
-alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
-alabama.virus_effects
-
-jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
-jersey.virus_effects
-
-california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
-california.virus_effects
-
-alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
-alaska.virus_effects
+# alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
+# alabama.virus_effects
+#
+# jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
+# jersey.virus_effects
+#
+# california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
+# california.virus_effects
+#
+# alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
+# alaska.virus_effects
 
 
 #=======================================================================
 # Reflection Section
+
+# What are the differences between the two different hash syntaxes shown in the state_data file?
+#     There are a few differences with the hashes, the most apparent being that one of the hash types is a variable to the other hash's keys.
+#     Additionally the inner hash utilized symbols since the key names used were repetative whereas the outer hash utilized strings as the key names since each one was unique.
+
+# What does require_relative do? How is it different from require?
+#     require_relative points to a file that is relative in position to the file in which it is being called.
+#     require seems to use the current directory from which you are running the program whereas require_relative uses the directory of where the program itself resides.
+#     requre_relative seems like a safer way of connecting files together for long term use.
+
+# What are some ways to iterate through a hash?
+#     We used the .each method to iterate thorugh each key of the main hash.
+#     There are other .each methods which iterate through only the keys (.each_key) or values (.each_value).
+#     The .each_pair submethod seems to work very similarly to the .each method.
+#     Some other ways to iterate through a hash, depending on what the intention is, may be .keep_if., .reject, .select
+
+# When refactoring virus_effects, what stood out to you about the variables, if anything?
+#     Since the class already had the information needed for the methods called stored as attributes of the class, it was not necessary to push them to the methods again. Attributes are available to all methods within the class.
+
+# What concept did you most solidify in this challenge?
+#     We had a hard time with accessing data in the sub hash. Although we had talked about how to access that data in the sub hash, since we were passing the sub hash as one variable it took us a while to click. 

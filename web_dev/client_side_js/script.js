a6be3fdc205b24228987f46c9507c4476e19009d
Sarah Suchá <sarah_deutschland@hotmail.com>
2016-06-27 19:10:40 -0700

    Refactor code with onclick listener in html and style options in css file

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,12 +1,8 @@
-var list = document.getElementById("nav-list");
-
-list.style.display="none";
-//document.getElementById("nav-list").style.display = "none";
-
-function revealMenu(){
-  var list = document.getElementById("nav-list");
-  list.style.display = "block";
+function buttonClick() {
+  var list = document.getElementById("list");
+  if (list.className == "menuOff"){
+    list.className = "menuOn";
+  } else {
+    list.className = "menuOff";
+  }
 }
-
-var hamburgerIcon = document.getElementById("hamburgerIcon");
-hamburgerIcon.addEventListener("click", revealMenu);
